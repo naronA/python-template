c1a4c28d41f5379317f9383dff26332d0a4269a9
tMinamiii <otherthan.new@gmail.com>
2021-09-09 20:15:23 +0900

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -Ceu
 
+if [ ! -e 'src' ]; then
+  mkdir src
+fi
 curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
 curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
 curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
@@ -14,6 +17,3 @@ curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D '$(cat requirements.txt)'
-if [ ! -e 'src' ]; then
-  mkdir src
-fi
