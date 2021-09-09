c77f27f5fe5d137cda1c9ff670f6c29af996cc5b
tMinamiii <otherthan.new@gmail.com>
2021-09-09 20:05:07 +0900

    silent

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -Ceu
 
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
+curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
+curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
+curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
+curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
+curl -s -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
+curl -s -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D "$(cat requirements.txt)"
