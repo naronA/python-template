c8bbff8fb63c9d6c651a27bbdf35e2cb463411f1
tMinamiii <otherthan.new@gmail.com>
2021-09-09 20:34:22 +0900

    update

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ set -Ceu
 if [ ! -e "src" ]; then
   mkdir src
 fi
-curl -sL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
-curl -sL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
-curl -sL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
-curl -sL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
-curl -sL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
-curl -sLO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
-curl -sLO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
-curl -sLO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
-curl -sLO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
-curl -sLO -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
+curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
+curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
+curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
+curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
+curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
+curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
+curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
+curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
+curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
+curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D $(cat requirements.txt)
