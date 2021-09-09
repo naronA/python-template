2891b282e96f6f67af2bf0b8573509bce99677ff
tMinamiii <otherthan.new@gmail.com>
2021-09-09 20:40:54 +0900

    update

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ set -Ceu
 if [ ! -e "src" ]; then
   mkdir src
 fi
-curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
-curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
-curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
-curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
-curl -sSfL -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
-curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
-curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
-curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
-curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
-curl -sSfLO -H "Cache-Control: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
+curl -sSfL -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
+curl -sSfL -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
+curl -sSfL -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
+curl -sSfL -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
+curl -sSfL -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
+curl -sSfLO -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/setup.cfg
+curl -sSfLO -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
+curl -sSfLO -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
+curl -sSfLO -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
+curl -sSfLO -H "Expires: 0" -H "Cache-Control: no-cache, no-store, must-revalidate" -H "Pragma: no-cache" https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
 python -m venv .venv
 poetry config --local virtualenvs.in-project true
 poetry add -D $(cat requirements.txt)
