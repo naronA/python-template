f45fecee7b8b6535d790d6f4fa6f9aea99465469
tMinamiii <otherthan.new@gmail.com>
2021-09-09 19:57:25 +0900

    update

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 set -Ceu
 
-curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/.vscode/settings.json --create-dirs --output .vscode/settings.json
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.env.workspace
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.envrc
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.flake8
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.gitignore
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.isort.cfg
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.mypy.ini
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.pydocstyle
-curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.pre-commit-config.yaml
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/vscode/settings.json --create-dirs -o .vscode/settings.json
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.env.workspace -o .env.workspace
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.envrc -o .envrc
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.flake8 -o .flake8
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.gitignore -o .gitignore
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.isort.cfg -o .isort.cfg
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pydocstyle -o .pydocstyle
+curl -L https://raw.githubusercontent.com/tMinamiii/python-template/master/dot.pre-commit-config.yaml -o .pre-commit-config.yaml
+# curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.mypy.ini
+curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyrightconfig.json
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/requirements.txt
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/editorconfig
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/pyproject.toml
