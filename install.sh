4678a1e17b4756fd75d95a9b18451ccd8cd9a064
tMinamiii <otherthan.new@gmail.com>
2021-09-09 19:40:02 +0900

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -Ceu
 
-# if [ ! -e "/.vscode" ]; then
-#   mkdir .vscode
-#   curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.vscode/settings.json -o .vscode/settings.json
-# fi
+if [ ! -e ".vscode" ]; then
+  mkdir .vscode
+  curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.vscode/settings.json -o .vscode/settings.json
+fi
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.env.workspace
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.envrc
 curl -L -O https://raw.githubusercontent.com/tMinamiii/python-template/master/.flake8
